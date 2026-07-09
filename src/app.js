6436184066a7bad653192bf52acecdb2957fba0d
joseerisvaldo <joseerisvaldopg@gmail.com>
2026-07-09 20:30:50 -0300

    feat: add auth

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 import express from "express";
+import cors from "cors";
 import subscriptionsWebHookRoute from "./routes/subscriptionsWebHook.js";
 import plansRoute from "./routes/plans.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
 app.use("/webhook", subscriptionsWebHookRoute);
 
+// Enable CORS for the frontend during development
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
+app.use("/auth", authRoutes);
 app.use("/plans", plansRoute);
 
 export default app;
